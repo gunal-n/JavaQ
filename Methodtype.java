a6cd20dcf6504762f0b4f1d5ca35a390fc9458a6
gunal-n <gunalkavithanedunchezian@gmail.com>
2025-04-23 14:00:03 +0530

    Initial commit

---
+++ Methodtype.java
@@ -0,0 +1,17 @@
+
+// //DATA TYPE WITHOUT PARAMETER//
+// import java.util.Scanner;
+// class Methodtype{
+//     public static int fact(int n){
+//         // System.err.println(n);
+//         return  n;
+//     }
+
+
+//     public static void main(String[] args){
+//         Scanner s = new Scanner(System.in);
+//         int n = s.nextInt();
+//         int b = fact(n);
+//         System.err.println(n);
+//     }
+// }
---
+++ patten.java
@@ -0,0 +1,173 @@
+//Square patten//
+// import java.util.Scanner;
+
+// public class patten {
+//     public static void main(String[] args) {
+//         Scanner s = new Scanner(System.in);
+//         int a = s.nextInt();
+        
+//         for(int i = 1; i <= a; i++){
+//             for(int j = 1; j <= a; j++){
+//                 if(j == 1 || j == a || i == 1 || i == a){
+//                     System.out.print("* ");
+//                 }
+//                 else{
+//                     System.out.print("  ");
+//                 }
+//             }
+//             System.err.println();
+//         }
+//     }
+// }
+
+//Rigth angle traingle//
+
+// import java.util.Scanner;
+
+//  class patten {
+//     public static void main(String[] args) {
+//         Scanner s = new Scanner(System.in);
+//         System.out.print("Enter the size: ");
+//         int a = s.nextInt();
+
+//         for (int i = 1; i <= a; i++) {
+//             for (int j = 1; j <= i; j++) {
+//                 if (j == 1 || i == a || j == i) {
+//                     System.out.print("* ");
+//                 } else {
+//                     System.out.print("  "); 
+//                 }
+//             }
+//             System.err.println();
+//         }
+//     }
+// }
+
+
+//Invert rigth//
+
+// import java.util.Scanner;
+
+// public class patten {
+//     public static void main(String[] args) {
+//         Scanner s = new Scanner(System.in);
+//         System.out.print("Enter the size: ");
+//         int a = s.nextInt();
+
+//         for (int i = a; i >= 1; i--) {
+//             for (int j = 1; j <= i; j++) {
+//                 if (j == 1 || j == i || i == a) {
+//                     System.out.print("* ");
+//                 } else {
+//                     System.out.print("  ");  
+//                 }
+//             }
+//             System.err.println();
+//         }
+//     }
+// }
+
+
+//+ patten//
+
+// import java.util.Scanner;
+
+// public class patten {
+//     public static void main(String[] args) {
+//         Scanner s = new Scanner(System.in);
+//         System.out.print("Enter odd size: ");
+//         int a = s.nextInt();
+
+//         int mid = (a % 2 == 0) ? (a / 2) : (a + 1) / 2; 
+
+//         for (int i = 1; i <= a; i++) {
+//             for (int j = 1; j <= a; j++) {
+//                 if (i == mid || j == mid) {
+//                     System.out.print("* ");
+//                 } else {
+//                     System.out.print("  ");
+//                 }
+//             }
+//             System.err.println();
+//         }
+//     }
+// }
+
+
+//X patten//
+
+// import java.util.Scanner;
+
+// public class patten {
+//     public static void main(String[] args) {
+//         Scanner s = new Scanner(System.in);
+//         int a = s.nextInt();
+
+//         for (int i = 1; i <= a; i++) {
+//             for (int j = 1; j <= a; j++) {
+//                 if (i == j || j == a - i + 1) {
+//                     System.out.print("* ");
+//                 } else {
+//                     System.out.print("  ");
+//                 }
+//             }
+//             System.err.println();
+//         }
+//     }
+// }
+
+//Right angle trangle//
+// import java.util.Scanner;
+
+//  class patten {
+//     public static void main(String[] args) {
+//         Scanner s = new Scanner(System.in);
+//         System.out.print("Enter the size: ");
+//         int a = s.nextInt();
+
+//         for (int i = 1; i <= a; i++) {
+//             for (int j = 1; j <= i; j++) {
+//                     System.out.print("* ");
+//             }
+//             System.err.println();
+//         }
+//     }
+// }
+
+//Inverst right//
+
+// import java.util.Scanner;
+
+//  class patten {
+//     public static void main(String[] args) {
+//         Scanner s = new Scanner(System.in);
+//         System.out.print("Enter the size: ");
+//         int a = s.nextInt();
+
+//         for (int i = 1; i <= a; i++) {
+//             for (int j = i; j <= a; j++) {
+//                     System.out.print("* ");
+//             }
+//             System.err.println();
+//         }
+//     }
+// }
+
+//Pramed //
+
+import java.util.Scanner;
+
+ class patten {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the size: ");
+        int a = s.nextInt();
+
+        for (int i = 1; i <= a; i++) {
+            for (int j = 1; j <= i; j++) {
+                    System.out.print("* ");
+            }
+            System.err.println();
+        }
+    }
+}
