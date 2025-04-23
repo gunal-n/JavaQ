1044446619638f1b8d73d35f2fccbdc6d162ea4d
gunal-n <gunalkavithanedunchezian@gmail.com>
2025-04-23 14:16:34 +0530

    Initial commit

--- patten.java
+++ patten.java
@@ -155,6 +155,28 @@
 
 //Pramed //
 
+// import java.util.Scanner;
+
+//  class patten {
+//     public static void main(String[] args) {
+//         Scanner s = new Scanner(System.in);
+//         System.out.print("Enter the size: ");
+//         int a = s.nextInt();
+
+//         for (int i = 1; i <= a; i++) {
+//             for (int j = i; j < a; j++) {
+//                     System.out.print(" ");
+//             }
+//             for(int k = 1; k <= (2 * i - 1); k++){
+//                 System.out.print("*");
+//             }
+//             System.err.println();
+//         }
+//     }
+// }
+
+//diman//
+
 import java.util.Scanner;
 
  class patten {
@@ -164,8 +186,20 @@ import java.util.Scanner;
         int a = s.nextInt();
 
         for (int i = 1; i <= a; i++) {
-            for (int j = 1; j <= i; j++) {
-                    System.out.print("* ");
+            for (int j = i; j < a; j++) {
+                    System.out.print(" ");
+            }
+            for(int k = 1; k <= (2 * i - 1); k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = a - 1; i >= 1; i--) {
+            for (int j = a; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
             }
             System.err.println();
         }
