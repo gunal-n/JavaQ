43d7d28f52e9dd2e35b4f54424cfa8914e8eec0d
gunal-n <gunalkavithanedunchezian@gmail.com>
2025-04-29 10:05:03 +0530

    first commit

---
+++ sum.java
@@ -0,0 +1,43 @@
+
+//mulpitable without * //
+// public class sum {
+//     public static void main(String[] args) {
+//         int n1 = 8;
+//         int n2 = 5;
+//         int sum = 0;
+//         for(int i = 1; i <= n2; i++){
+//              sum = sum + n1;
+//         }
+//         System.out.println(sum);
+//     }
+// }
+
+//divided without / //
+
+// public class sum{
+//     public static void main(String[] args) {
+//         int largenumber = 20;
+//         int divided = 5;
+//         int q = 0;
+//         while(largenumber >= divided){
+//             q++;
+//             largenumber = largenumber - divided;
+//         }
+//         System.out.println(q);
+//     }
+// }
+
+// odd and even without bit and % //
+
+// public class sum{
+//     public static void main(String[] args) {
+//         int n = 70;
+//         if((n/2)*2 == n){
+//             System.out.println("Even");
+//         }
+//         else{
+//             System.out.println("Odd");
+//         }
+//     }
+// }
+
